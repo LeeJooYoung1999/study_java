db1c51d5e5498ac30e75986f91a1eb45f0808eb0
LeeJooYoung1999 <ptljoo1572@gmail.com>
2025-05-12 16:04:04 +0900

    06_중첩객체(기본)

--- 06_중첩객체(기본)/ch09/sec03/exam01/AExample.java
+++ 06_중첩객체(기본)/ch09/sec03/exam01/AExample.java
@@ -1,4 +1,8 @@
 package ch09.sec03.exam01;
 //정적 내부 멤버 클래스 B의 인스턴스를 생성하세요
 public class AExample {
+    public static void main(String[] args) {
+        //B 객체 생성
+        A.B b = new A.B();
+    }
 }
--- 06_중첩객체(기본)/ch09/sec07/exam02/RemoteControl.java
+++ 06_중첩객체(기본)/ch09/sec07/exam02/RemoteControl.java
@@ -1,4 +1,6 @@
 package ch09.sec07.exam02;
-
-public class RemoteControl {
+public interface RemoteControl {
+    //추상 메소드
+    void turnOn();
+    void turnOff();
 }
